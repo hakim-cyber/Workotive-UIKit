196705656819ae86e7e3c01b012ef6320a7f79c0
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-08-15 23:30:37 +0400

    now have func for having exercises

--- Workoutive Uikit/ViewModels/DataManager.swift
+++ Workoutive Uikit/ViewModels/DataManager.swift
@@ -40,4 +40,46 @@ class DataManager:ObservableObject{
             self.days[index].muscles.removeAll(where: {$0.id == muscle.id})
         }
     }
+    func loadAllExcercises(for bodyPart:String,completion:@escaping([ExerciseApi])-> Void){
+        let saveKeyExercises = "exercises"
+        var exercises = [ExerciseApi]()
+        
+        if let savedData = UserDefaults.standard.data(forKey: saveKeyExercises){
+            // if value nothing to do
+            if let decoded = try? JSONDecoder().decode([ExerciseApi].self, from: savedData){
+                exercises = decoded
+            }
+        }else{
+            let headers = [
+                "content-type": "application/octet-stream",
+                "X-RapidAPI-Key": "d3be8ad012mshea31fdf3fc52d3bp18251bjsn0ff8b0e32a60",
+                "X-RapidAPI-Host": "exercisedb.p.rapidapi.com"
+            ]
+            
+            guard let url = URL(string: "https://exercisedb.p.rapidapi.com/exercises") else{return}
+            
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.allHTTPHeaderFields = headers
+            
+            
+            
+            
+            URLSession.shared.dataTask(with: request as URLRequest){data,_,error in
+                guard let data = data else{return}
+                if let decoded = try?JSONDecoder().decode([ExerciseApi].self, from: data){
+                    DispatchQueue.main.async {
+                        exercises = decoded
+                        print(exercises.first?.name)
+                        if let encoded = try? JSONEncoder().encode(exercises){
+                            UserDefaults.standard.set(encoded, forKey: saveKeyExercises)
+                        }
+                    }
+                    
+                }
+            }.resume()
+            
+        }
+        completion(exercises.filter{$0.bodyPart == bodyPart})
+    }
 }
