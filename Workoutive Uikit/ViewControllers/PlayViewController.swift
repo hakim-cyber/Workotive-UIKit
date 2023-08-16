3c563eafa6c357dd6aa39c7b1c28ed30f10a93d6
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-08-17 00:40:00 +0400

    nice made back Button

--- Workoutive Uikit/ViewControllers/PlayViewController.swift
+++ Workoutive Uikit/ViewControllers/PlayViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class PlayViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .openGreen
-
+        setup()
+        
         // Do any additional setup after loading the view.
     }
+    private lazy var backBtn:UIButton = {
+        let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        let largeConfig = UIImage.SymbolConfiguration(pointSize: 20, weight: .bold, scale: .default)
+               
+        let largeBoldX = UIImage(systemName: "xmark", withConfiguration: largeConfig)
+
+        
+        btn.setImage(largeBoldX, for: .normal)
+        btn.tintColor = .black
+        btn.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        
+        
+        return btn
+    }()
     
+    func setup(){
+        setupNavigationHeader()
+        
+    }
+    func setupNavigationHeader(){
+        
+        self.view.addSubview(backBtn)
+        
+        NSLayoutConstraint.activate([
+           
+            backBtn.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            backBtn.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 27)
+        
+        ])
+    }
+    
+    
+    //Buttons
+    @objc func backButtonTapped(){
+        self.dismiss(animated: true)
+    }
 }
