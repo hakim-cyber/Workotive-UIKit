c6f9fb02610b062748184d55b950c4da82ccf722
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-08-16 15:29:42 +0400

    Everything successful now

--- Workoutive Uikit/ViewModels/DataManager.swift
+++ Workoutive Uikit/ViewModels/DataManager.swift
@@ -110,6 +110,191 @@ class DataManager:ObservableObject{
             
         
     }
+    /*
+    func updateDay(day:Day)async{
+            guard let appdelegate = await UIApplication.shared.delegate as? AppDelegate else { return }
+            let managedContext = await appdelegate.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "CashedDay")
+            fetchRequest.predicate = NSPredicate(format: "id = %d", Int16(day.id))
+
+
+            do{
+                
+                var test = try managedContext.fetch(fetchRequest) as! [CashedDay]
+                
+              
+                if test.count > 0{
+                    
+                    
+                    
+                    var arrayMuscles = Set<CashedMuscle>()
+                    
+                    for muscle in day.muscles{
+                        let newMuscle = CashedMuscle(context: managedContext)
+                        newMuscle.muscle = muscle.muscle
+                        newMuscle.id = muscle.id
+                        
+                        var arrayExcercise = Set<CashedExcercise>()
+                        
+                        for excercise in muscle.exercises{
+                            let newExercise = CashedExcercise(context: managedContext)
+                            newExercise.id = excercise.id
+                            newExercise.bodyPart = excercise.bodyPart
+                            newExercise.equipment = excercise.equipment
+                            newExercise.gifUrl = excercise.gifUrl
+                            newExercise.name = excercise.name
+                            newExercise.repeatsCount = Int16(excercise.repeatCount!)
+                            newExercise.setsCount = Int16(excercise.sets!)
+                            newExercise.target = excercise.target
+                            
+                            arrayExcercise.insert(newExercise)
+                        }
+                        newMuscle.exercise = arrayExcercise as NSSet
+                        
+                        arrayMuscles.insert(newMuscle)
+                    }
+                    print(day)
+                    test[0].muscle = arrayMuscles as NSSet
+                    
+                   
+                    
+                }
+               
+                do{
+                    try managedContext.save()
+                    print("saved \(test[0])")
+                }catch{
+                    print(error)
+                }
+                
+            }catch{
+                print(error)
+            }
+            
+        }
+       */
+    func updateDay(day: Day) async {
+        guard let appdelegate = await UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = await appdelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "CashedDay")
+        fetchRequest.predicate = NSPredicate(format: "id = %d", Int16(day.id))
+
+        do {
+            let cachedDays = try managedContext.fetch(fetchRequest) as! [CashedDay]
+
+            if let cachedDay = cachedDays.first {
+                var existingMuscles = cachedDay.muscle?.allObjects as? [CashedMuscle] ?? []
+                var updatedMuscles = Set<CashedMuscle>()
+
+                for muscle in day.muscles {
+                    if let existingMuscle = existingMuscles.first(where: { $0.id == muscle.id }) {
+                        // Update existing muscle properties
+                        existingMuscle.muscle = muscle.muscle
+                        existingMuscle.id = muscle.id
+
+                        // Update exercises
+                        var updatedExercises = Set<CashedExcercise>()
+
+                        for exercise in muscle.exercises {
+                            if let existingExercise = existingMuscle.exercise?.first(where: { ($0 as! CashedExcercise).id == exercise.id }) as? CashedExcercise {
+                                // Update existing exercise properties
+                                existingExercise.id = exercise.id
+                                existingExercise.bodyPart = exercise.bodyPart
+                                existingExercise.equipment = exercise.equipment
+                                existingExercise.gifUrl = exercise.gifUrl
+                                existingExercise.name = exercise.name
+                                existingExercise.repeatsCount = Int16(exercise.repeatCount!)
+                                existingExercise.setsCount = Int16(exercise.sets!)
+                                existingExercise.target = exercise.target
+                                // Update other exercise properties...
+
+                                updatedExercises.insert(existingExercise)
+                            }else{
+                                let newExercise = CashedExcercise(context: managedContext)
+                                newExercise.id = exercise.id
+                                newExercise.bodyPart = exercise.bodyPart
+                                newExercise.equipment = exercise.equipment
+                                newExercise.gifUrl = exercise.gifUrl
+                                newExercise.name = exercise.name
+                                newExercise.repeatsCount = Int16(exercise.repeatCount!)
+                                newExercise.setsCount = Int16(exercise.sets!)
+                                newExercise.target = exercise.target
+                                
+                                updatedExercises.insert(newExercise)
+                            }
+                        }
+
+                        existingMuscle.exercise = updatedExercises as NSSet
+                        updatedMuscles.insert(existingMuscle)
+                    } else {
+                        let newMuscle = CashedMuscle(context: managedContext)
+                        newMuscle.muscle = muscle.muscle
+                        newMuscle.id = muscle.id
+
+                        var arrayExcercise = Set<CashedExcercise>()
+
+                        for exercise in muscle.exercises {
+                            let newExercise = CashedExcercise(context: managedContext)
+                            newExercise.id = exercise.id
+                            newExercise.bodyPart = exercise.bodyPart
+                            newExercise.equipment = exercise.equipment
+                            newExercise.gifUrl = exercise.gifUrl
+                            newExercise.name = exercise.name
+                            newExercise.repeatsCount = Int16(exercise.repeatCount!)
+                            newExercise.setsCount = Int16(exercise.sets!)
+                            newExercise.target = exercise.target
+                            // Set other exercise properties...
+
+                            arrayExcercise.insert(newExercise)
+                        }
+
+                        newMuscle.exercise = arrayExcercise as NSSet
+                        updatedMuscles.insert(newMuscle)
+                    }
+                }
+
+                cachedDay.muscle = updatedMuscles as NSSet
+
+                do {
+                    try managedContext.save()
+                    print("Data saved successfully.")
+                } catch let error as NSError {
+                    print("Error saving data: \(error), \(error.userInfo)")
+                }
+            }
+        } catch {
+            print(error)
+        }
+    }
+
+
+    
+    func deleteFromCoreData(id:Int)async{
+        guard let appdelegate = await UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = await appdelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "CashedDay")
+        
+        do{
+            var test = try managedContext.fetch(fetchRequest)
+            
+            for data in test as! [CashedDay]{
+                if Int(data.id) == id{
+                    managedContext.delete(data)
+                    
+                    do{
+                        try managedContext.save()
+                    }catch{
+                        print(error)
+                    }
+                }
+            }
+           
+           
+          
+        }catch{
+            print(error)
+        }
+    }
     func addDays(day:Day) async{
        
             self.days.append(day)
@@ -120,22 +305,33 @@ class DataManager:ObservableObject{
        
         self.days.removeAll(where: {$0.id == day.id})
       
-        
+        Task{
+            await deleteFromCoreData(id:day.id)
+        }
     }
     func addMuscleTo(dayID:Int,muscle:Muscle)  {
         if let index = self.days.firstIndex(where: {$0.id == dayID}){
             if let indexOfExistingMuscle = self.days[index].muscles.firstIndex(where: {$0.id == muscle.id}){
                 self.days[index].muscles[indexOfExistingMuscle] = muscle
                 print("adding existing muscle")
+                
             }else{
                 self.days[index].muscles.append(muscle)
+               
             }
-           
+            
+            Task{
+                print("\(self.days[index])")
+                await updateDay(day: self.days[index])
+            }
         }
     }
     func deleteMuscleAt(dayID:Int,muscle:Muscle) {
         if let index = self.days.firstIndex(where: {$0.id == dayID}){
             self.days[index].muscles.removeAll(where: {$0.id == muscle.id})
+            Task{
+                await updateDay(day: self.days[index])
+            }
            
         }
     }
